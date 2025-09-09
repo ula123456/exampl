93e81b32fa4223b2c793ffa32b180234b56e8abf
unknown <umeylikov93@gmail.com>
2025-09-09 19:06:48 +0500

    update Signed-off-by: unknown <umeylikov93@gmail.com>

--- private/controllers/home.php
+++ private/controllers/home.php
@@ -13,15 +13,15 @@ class Home extends Controller
 		$user = new User();
 		
 
-			$arr['firstname'] = 'alim';
-        	$arr['lastname'] = 'babam';
-        	$arr['date'] = '2025-09-09 07:32:18';
-            $arr['url_adress'] = 'fgfg';
-            $arr['gender'] = 'female';
-            $arr['school_id'] = '12';
-            $arr['rang'] = 'student';
-
-		$user->insert($arr);
+			$arr['firstname'] = 'alim111';
+        	$arr['lastname'] = 'babam111';
+        	//$arr['date'] = '2025-09-09 07:32:18';
+            //$arr['url_adress'] = 'fgfg';
+            //$arr['gender'] = 'female';
+            //$arr['school_id'] = '12';
+            //$arr['rang'] = 'student';
+			$user->update(1,$arr);
+		//$user->insert($arr);
 		$data = $user->findAll();
 		//$data = $user->where('firstname','ali');
 		$this->view('home',['rows'=>$data]);
--- private/core/model.php
+++ private/core/model.php
@@ -47,6 +47,19 @@ class Model extends Database
 	public function update($id,$data)
 	{
 		
+		
+		$str = "";
+		foreach ($data as $key => $value) {
+			// code...
+			$str .=$key. "=:".$key.",";
+		}
+		
+		$str = trim($str,",") ;
+		
+		$data['id'] = $id;
+		$query="update $this->table set $str where id = :id";
+
+		return $this->query($query,$data);
 	}
 	public function delete($id)
 	{
